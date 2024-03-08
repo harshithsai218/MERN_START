11175238c7ffb26784a8277b6a365bb6abb933f5
harshithsai218 <harshithsai2004@gmail.com>
2024-03-08 22:55:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter as Router ,Route} from "react-router-dom";
+import Users from './Users/Pages/Users';
+const App = () =>{
+    return <Router>
+        <Route path='/' exact>
+          <Users/> 
+        </Route>;
+    </Router>;
+};
+
+export default App;

d856f9108256a7b33e450533c133088c0dcefbb3
harshithsai218 <harshithsai2004@gmail.com>
2024-03-09 01:46:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {BrowserRouter as Router ,Route, Redirect} from "react-router-dom";
+import {BrowserRouter as Router ,Route, Redirect,Switch} from "react-router-dom";
 import Users from './Users/Pages/Users';
+import NewPlaces from './Places/Pages/NewPlaces';
 const App = () =>{
     return <Router>
+      <Switch>
         <Route path='/' exact>
           <Users/> 
         </Route>;
+        <Route path='/places/new' exact>
+          <NewPlaces/> 
+        </Route>;
         <Redirect to = '/' />;
+        </Switch> 
     </Router>;
 }; 
 
---
+++ src/Users/Components/UserList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './UserList.css'
+
+const UserList = props=>{
+    
+};
+
+export default UserList;

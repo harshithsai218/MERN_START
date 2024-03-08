5d15bb78341cfc6d0cbf68b3abd5c8aa887e9992
harshithsai218 <harshithsai2004@gmail.com>
2024-03-08 22:57:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {BrowserRouter as Router ,Route} from "react-router-dom";
+import {BrowserRouter as Router ,Route, Redirect} from "react-router-dom";
 import Users from './Users/Pages/Users';
 const App = () =>{
     return <Router>
         <Route path='/' exact>
           <Users/> 
         </Route>;
+        <Redirect to = '/' />;
     </Router>;
 };
 

5b03e295d0e82b103a0b1be077f5f6b440b9bfc5
harshithsai218 <harshithsai2004@gmail.com>
2024-03-09 13:41:11 +0530

    need to correct with diraizelet

--- src/Users/Components/UserItem.js
+++ src/Users/Components/UserItem.js
@@ -1,20 +1,27 @@
 import React  from "react";
 import "./UserItem.css";
+import Avatar from "../../Shared/Components/UIElements/Avatar";
+import Card from "../../Shared/Components/UIElements/Card";
+import {Link} from 'react-router-dom';
 
 const UserItem = props => {
     return(
         <li className="user-list">
-            <div className="user-item__content">
+            
+                <Card className="user-item__content">
+                <Link to={`/${props.id}/places`}>
                 <div className="user-item__image">
-                    <img src = {props.image} alt={props.name}/>
+                    <Avatar image={props.image} alt = {props.name}/>
                 </div>
                 <div className="user-item__info">
                     <h2>{props.name}</h2>
                     <h3>
-                        {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+                        {props.placeCount} {props.placeCount===1 ? 'place' :'places'}
                     </h3>
                 </div>
-            </div>
+            </Link>
+            </Card>
+            
         </li>
     );
 };

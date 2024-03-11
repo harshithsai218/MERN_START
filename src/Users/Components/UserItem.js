555ed6d47c4105aa22f24f237eb3ef3131becfd9
harshithsai218 <harshithsai2004@gmail.com>
2024-03-11 12:14:36 +0530

    nav bar still not corrected but align and bullet is removed

--- src/Users/Components/UserItem.js
+++ src/Users/Components/UserItem.js
@@ -7,21 +7,21 @@ import Card from "../../Shared/Components/UIElements/Card";
 import "./UserItem.css";
 
 const UserItem = props => {
-    return(
+    return (
         <li className="user-item">
+            
                 <Card className="user-item__content">
                 <Link to={`/${props.id}/places`}>
                 <div className="user-item__image">
-                    <Avatar image={props.image} alt = {props.name}/>
+                    <Avatar image= {props.image} alt ={props.name} />  
                 </div>
                 <div className="user-item__info">
                     <h2>{props.name}</h2>
-                    <h3>
-                        {props.placeCount} {props.placeCount===1 ? 'place' :'places'}
-                    </h3>
+                    <h3>{props.placeCount} {props.placeCount === 1 ? 'Place': 'Places'}</h3>
                 </div>
-            </Link>
-            </Card>
+                </Link>
+                </Card>
+            
         </li>
     );
 };
--- src/Users/Components/UserList.js
+++ src/Users/Components/UserList.js
@@ -17,7 +17,7 @@ const UserList = props=>{
         );
     }
     return (
-        <ul className='user-list'>
+        <ul className="user-list">
             {props.items.map(user => (
                 <UserItem
                 key={user.id}
@@ -32,3 +32,36 @@ const UserList = props=>{
 };
 
 export default UserList;
+/*
+import React from 'react';
+import UserItem from './UserItem';
+import Card from '../../Shared/Components/UIElements/Card';
+import './UserList.css'; // Importing the CSS file
+
+const UserList = (props) => {
+    if (props.items.length === 0) {
+        return (
+            <div className="center">
+                <Card>
+                    <h2>No Users Found!</h2>
+                </Card>
+            </div>
+        );
+    }
+    return (
+        <ul className="users-list"> 
+            {props.items.map(user => (
+                <UserItem
+                    key={user.id}
+                    id={user.id}
+                    image={user.image}
+                    name={user.name}
+                    placeCount={user.placeCount}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default UserList;
+*/
